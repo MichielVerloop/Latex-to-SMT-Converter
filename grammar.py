a05d0fe2bf3f7c49bba9d660b315f6ed4e17be38
MichielVerloop <59741046+MichielVerloop@users.noreply.github.com>
2022-11-17 12:08:31 +0000

    Update parsimonious to 0.10.0
    • Fix `rexpr")"` not being parsed correctly in the new version of parsimonious
      ○ The fix was to add a space between the reference and the string literal.
    • Convert strings in grammar.py to raw strings.

--- grammar.py
+++ grammar.py
@@ -5,45 +5,45 @@ from parsimonious.grammar import Grammar
 # TODO: negative integers
 class BoolGrammar:
     latex_grammar = Grammar(
-        """
+        r"""
     expr            = wedge / rwedge / vee / rvee / sum / rsum / ite / not / varint / nexpr
-    nexpr           = "("expr rexpr")"
+    nexpr           = "(" expr rexpr ")"
     rexpr           = and / or / implies / equals / neq / le / leq / ge / geq / plus / minus / times / varint
-    ite             = "\\\\T{if}" expr "\\\\T{then}" expr "\\\\T{else}" expr
-    wedge           = "\\\\bigwedge_{" lower "}" "^{" reduciblevarint "}" expr
-    vee             = "\\\\bigvee_{" lower "}" "^{" reduciblevarint "}" expr
-    rwedge          = "\\\\bigwedge_{" lowup "}" expr
-    rvee            = "\\\\bigvee_{" lowup "}" expr
-    sum             = "\\\\sum_{" lower "}" "^{" reduciblevarint "}" expr
-    rsum            = "\\\\sum_{" lowup "}" expr
+    ite             = "\\T{if}" expr "\\T{then}" expr "\\T{else}" expr
+    wedge           = "\\bigwedge_{" lower "}" "^{" reduciblevarint "}" expr
+    vee             = "\\bigvee_{" lower "}" "^{" reduciblevarint "}" expr
+    rwedge          = "\\bigwedge_{" lowup "}" expr
+    rvee            = "\\bigvee_{" lowup "}" expr
+    sum             = "\\sum_{" lower "}" "^{" reduciblevarint "}" expr
+    rsum            = "\\sum_{" lowup "}" expr
     lower           = localvar "=" reduciblevarint
-    lowup           = localvars ":" reduciblevarint ("\\\\leq" / "<") (localvar ("\\\\leq" / "<"))+ reduciblevarint
+    lowup           = localvars ":" reduciblevarint ("\\leq" / "<") (localvar ("\\leq" / "<"))+ reduciblevarint
     localvars       = localvar (","localvar)*
     reduciblevarint = (var / int) (("+" / "-") (var / int))*
     varint          = var / replaceablevar / int
     int             = (~"[0-9]"* ".")? ~"[0-9]"+
     localvar        = ~"[a-z_]+"i
-    replaceablevar  = "\\\\markreplaceable{" var "}"
+    replaceablevar  = "\\markreplaceable{" var "}"
     var             = (string "_{" substring ("," substring)* "}") / (string "_" (string/int)) / (string)
     substring       = (string / int) (("+" / "-") (string / int))* 
     string          = ~"[a-z]+"i
-    and             = ("\\\\land" expr)+
-    or              = ("\\\\lor" expr)+
-    implies         = ("\\\\to" expr)+
+    and             = ("\\land" expr)+
+    or              = ("\\lor" expr)+
+    implies         = ("\\to" expr)+
     equals          = ("=" expr)+
-    neq             = ("\\\\neq" expr)+
-    not             = "\\\\lnot" expr
+    neq             = ("\\neq" expr)+
+    not             = "\\lnot" expr
     le              = ("<" expr)+
-    leq             = ("\\\\leq" expr)+
+    leq             = ("\\leq" expr)+
     ge              = (">" expr)+
-    geq             = ("\\\\geq" expr)+
+    geq             = ("\\geq" expr)+
     plus            = ("+" expr)+
     minus           = ("-" expr)+
-    times           = ("\\\\cdot" expr)+ 
+    times           = ("\\cdot" expr)+ 
     """)
 
     global_grammar = Grammar(
-        """
+        r"""
     assignments     = equals ("," equals)*
     equals          = var "=" varint
     varint          = (var / int) (pmvarint)*
@@ -57,7 +57,7 @@ class BoolGrammar:
 
     # TODO: update this with the "num" definition rather than int.
     reducible_varint_grammar = Grammar(
-        """
+        r"""
     varint          = (var / int) (pmvarint)*
     pmvarint        = ("+" / "-") (var / int)
     var             = (string "_{" substring ("," substring)* "}") / (string "_" (string/int)) / (string)
@@ -68,15 +68,15 @@ class BoolGrammar:
     )
 
     operand_dict = {
-        "distinct": ("\\neq", unknown, boolean),
+        "distinct": (r"\neq", unknown, boolean),
         "=": ("=", unknown, boolean),
-        "not": ("\\lnot", boolean, boolean),
-        "and": ("\\land", boolean, boolean),
-        "or": ("\\lor", boolean, boolean),
-        "=>": ("\\to", boolean, boolean),
-        "<=": ("\\leq", num, boolean),
-        ">=": ("\\geq", num, boolean),
-        "*": ("\\cdot", num, num),
+        "not": (r"\lnot", boolean, boolean),
+        "and": (r"\land", boolean, boolean),
+        "or": (r"\lor", boolean, boolean),
+        "=>": (r"\to", boolean, boolean),
+        "<=": (r"\leq", num, boolean),
+        ">=": (r"\geq", num, boolean),
+        "*": (r"\cdot", num, num),
         "<": ("<", num, boolean),
         ">": (">", num, boolean),
         "+": ("+", num, num),
@@ -84,20 +84,20 @@ class BoolGrammar:
     }
 
     inverse_operand_dict = {
-        "\\bigwedge": ("and", boolean, boolean),
-        "\\bigvee": ("or", boolean, boolean),
-        "\\sum": ("+", num, num),
-        "\\neq": ("distinct", unknown, boolean),
-        "=": ("=", unknown, boolean),
-        "\\lnot": ("not", boolean, boolean),
-        "\\land": ("and", boolean, boolean),
-        "\\lor": ("or", boolean, boolean),
-        "\\to": ("=>", boolean, boolean),
-        "\\leq": ("<=", num, boolean),
-        "\\geq": (">=", num, boolean),
-        "\\cdot": ("*", num, num),
-        "<": ("<", num, boolean),
-        ">": (">", num, boolean),
-        "+": ("+", num, num),
-        "-": ("-", num, num)
+        r"\bigwedge": ("and", boolean, boolean),
+        r"\bigvee": ("or", boolean, boolean),
+        r"\sum": ("+", num, num),
+        r"\neq": ("distinct", unknown, boolean),
+        r"=": ("=", unknown, boolean),
+        r"\lnot": ("not", boolean, boolean),
+        r"\land": ("and", boolean, boolean),
+        r"\lor": ("or", boolean, boolean),
+        r"\to": ("=>", boolean, boolean),
+        r"\leq": ("<=", num, boolean),
+        r"\geq": (">=", num, boolean),
+        r"\cdot": ("*", num, num),
+        r"<": ("<", num, boolean),
+        r">": (">", num, boolean),
+        r"+": ("+", num, num),
+        r"-": ("-", num, num)
     }
